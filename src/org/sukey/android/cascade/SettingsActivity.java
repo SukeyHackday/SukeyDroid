a55e53f4b54e5e40ac505440395f6cc641e3461c
Dave Ingram <dmi@groupspaces.com>
2011-02-20 20:27:20 +0000

    Initial import

---
+++ src/org/sukey/android/cascade/SettingsActivity.java
@@ -0,0 +1,13 @@
+package org.sukey.android.cascade;
+
+import android.R;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+    }
+}
